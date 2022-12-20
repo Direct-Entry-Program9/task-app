598371d1364994d3d31f2e23f4dfabfcfea0737b
manelka <nishanmanelka@gmail.com>
2022-12-20 15:01:00 +0530

    Add ConnectionUtil

--- src/main/java/lk/ijse/dep9/app/entity/User.java
+++ src/main/java/lk/ijse/dep9/app/entity/User.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
