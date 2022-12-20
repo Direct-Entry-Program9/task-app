35de3cad31364fbacf84ae0a9e7bcd4db707716e
manelka <nishanmanelka@gmail.com>
2022-12-20 17:48:38 +0530

    Finalize dao layer implementation

--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
@@ -5,7 +5,8 @@ import lk.ijse.dep9.app.dao.custom.UserDAO;
 import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.User;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,36 +21,106 @@ public class ProjectDAOImpl implements ProjectDAO {
 
     @Override
     public Project save(Project project) {
-        return null;
+        try {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO Project (name, username) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, project.getName());
+            stm.setString(2, project.getUsername());
+            stm.executeUpdate();
+            ResultSet rst = stm.getGeneratedKeys();
+            rst.next();
+            project.setId(rst.getInt(1));
+            return project;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(Project project) {
-
+        try {
+            PreparedStatement stm = connection.prepareStatement("UPDATE Project SET name=? AND username=? WHERE id=?");
+            stm.setString(1,project.getName());
+            stm.setString(2,project.getUsername());
+            stm.setInt(3,project.getId());
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteById(Integer pk) {
-
+        try {
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM Project WHERE id=?");
+            stm.setInt(1,pk);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Project> findById(Integer pk) {
-        return Optional.empty();
+
+        try {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Project WHERE id=?");
+            stm.setInt(1,pk);
+            ResultSet rst = stm.executeQuery();
+            if (rst.next()){
+                return Optional.of(new Project(rst.getInt("id"),rst.getString("name"),rst.getString("username")));
+            }
+
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Project> findAll() {
-        return null;
+        try {
+            List<Project> projectList = new ArrayList<>();
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Project");
+            ResultSet rst = stm.executeQuery();
+            while (rst.next()){
+                projectList.add(new Project(rst.getInt("id"),rst.getString("name"),rst.getString("username")));
+            }
+            return projectList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public long count() {
-        return 0;
+        try {
+            PreparedStatement stm = connection.prepareStatement("SELECT COUNT(id) FROM Project");
+            ResultSet rst = stm.executeQuery();
+            rst.next();
+            return rst.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean existsBYId(Integer pk) {
-        return false;
+        return findById(pk).isPresent();
+    }
+
+    @Override
+    public List<Project> findAllProjectsByUsername(String username) {
+        try {
+            List<Project> projectList = new ArrayList<>();
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Project WHERE username=?");
+            stm.setString(1,username);
+            ResultSet rst = stm.executeQuery();
+            while (rst.next()){
+                projectList.add(new Project(rst.getInt("id"),rst.getString("name"),rst.getString("username")));
+            }
+            return projectList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
