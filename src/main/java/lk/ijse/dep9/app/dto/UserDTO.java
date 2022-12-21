a3925da7af424ee6b0071c07efd891f91a066b3f
manelka <nishanmanelka@gmail.com>
2022-12-21 11:50:52 +0530

    Handle exception

--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -3,13 +3,22 @@ package lk.ijse.dep9.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO implements Serializable {
+    @NotBlank(message = "full_name can't be empty or null")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name")
     private String fullName;
+    @NotBlank(message = "username can't be empty or null")
     private String username;
+    @NotEmpty(message = "Password can't be empty or null")
+    @Length(min = 3, message = "Password should be at least 3 characters long")
     private String password;
 }
