d186db71c8d8236306e84a26e37adb9ad6c95293
manelka <nishanmanelka@gmail.com>
2022-12-22 13:54:44 +0530

    Add ProjectDTO

---
+++ src/main/java/lk/ijse/dep9/app/dto/ProjectDTO.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep9.app.dto;
+
+import lk.ijse.dep9.app.util.ValidationGroup;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDTO implements Serializable {
+    @Null(groups = ValidationGroup.Create.class, message = "Project id can't be specified")
+    private Integer id;
+    @NotBlank(message = "Project name cannot be empty or null")
+    @Length(min = 3, message = "Project name should be at least 3 characters long")
+    private String name;
+}
